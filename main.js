849eeaf5c35cce30aa2554889cffb71acfa4d5f6
dejong1995 <rodneydejong@me.com>
2020-11-04 19:55:53 +0100

    Decreased code length

--- main.js
+++ main.js
@@ -1,40 +1,5 @@
 let form = document.getElementById('form');
 
-function getFlag (response) {
-    return "<img src=\"" + response.data[0].flag + "\" alt=\"Flag of " + response.data[0].name + "\">";
-    }
-
-function generalString (response) {
-    return response.data[0].name + " is situated in " + response.data[0].subregion + "<br>It has a population "
-        +"of " + response.data[0].population + " people<br>The capital is " + response.data[0].capital;
-}
-
-function currencyString (response) {
-    let str = " and you can pay with ";
-    if (response.data[0].currencies.length === 1) {
-        str = str + response.data[0].currencies[0].name + "'s";
-    } if (response.data[0].currencies.length === 2) {
-        str = str + response.data[0].currencies[0].name + "'s and " + response.data[0].currencies[1].name + "'s";
-    }if (response.data[0].currencies.length > 2) {
-        for (let i = 0; i < response.data.currencies.name.length - 2; i++) {
-            str = str + response.data[0].currencies[i].name + "'s , ";
-        } str = str + response.data[0].currencies[-2].name + "'s and " + response.data[0].currencies[-1].name + "'s";
-    } return str;
-}
-
-function languageString (response) {
-    let str = "<br>They speak ";
-    if (response.data[0].languages.length === 1) {
-        str = str + response.data[0].languages[0].name;
-    }
-    if (response.data[0].languages.length === 2) {
-        str = str + response.data[0].languages[0].name + " and " + response.data[0].languages[1].name;
-    } if (response.data[0].languages.length === 3) {
-        str = str + response.data[0].languages[0].name + ", " + response.data[0].languages[1].name + " and " +
-            response.data[0].languages[2].name;
-    } return str;
-}
-
 //Fetching the keywords from the input field
 form.addEventListener('submit', async function getCountry(event) {
     event.preventDefault();
@@ -43,11 +8,12 @@ form.addEventListener('submit', async function getCountry(event) {
 
     try {
         const response = await axios.get('https://restcountries.eu/rest/v2/name/' + keywords);
-        const flag = getFlag(response);
+        const country = response.data[0];
+        const flag = getFlag(country);
         const name = response.data[0].name;
-        let str = generalString(response);
-        str = str + currencyString(response);
-        str = str + languageString(response);
+        let str = generalString(country);
+        str = str + currencyString(country);
+        str = str + languageString(country);
 
         document.getElementById("info").style.boxShadow = '0 0 20px rgba(0,0,0,0.8)';
         document.getElementById("flag").innerHTML = flag;
@@ -59,3 +25,39 @@ form.addEventListener('submit', async function getCountry(event) {
         document.getElementById("information").innerHTML = "Nothing found! Check your spelling.";
     } document.getElementById("form").reset();
 });
+
+
+function getFlag (response) {
+    return "<img src=\"" + response.flag + "\" alt=\"Flag of " + response.name + "\">";
+    }
+
+function generalString (response) {
+    return response.name + " is situated in " + response.subregion + "<br>It has a population "
+        +"of " + response.population + " people<br>The capital is " + response.capital;
+}
+
+function currencyString (response) {
+    let str = " and you can pay with ";
+    if (response.currencies.length === 1) {
+        str = str + response.currencies[0].name + "'s";
+    } if (response.currencies.length === 2) {
+        str = str + response.currencies[0].name + "'s and " + response.currencies[1].name + "'s";
+    }if (response.currencies.length > 2) {
+        for (let i = 0; i < response.currencies.name.length - 2; i++) {
+            str = str + response.currencies[i].name + "'s , ";
+        } str = str + response.currencies[-2].name + "'s and " + response.currencies[-1].name + "'s";
+    } return str;
+}
+
+function languageString (response) {
+    let str = "<br>They speak ";
+    if (response.languages.length === 1) {
+        str = str + response.languages[0].name;
+    }
+    if (response.languages.length === 2) {
+        str = str + response.languages[0].name + " and " + response.languages[1].name;
+    } if (response.languages.length === 3) {
+        str = str + response.languages[0].name + ", " + response.languages[1].name + " and " +
+            response.languages[2].name;
+    } return str;
+}
